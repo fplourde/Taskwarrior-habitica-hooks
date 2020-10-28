d07f6907e672a3f7932353e6e17e1690775ef950
Michael Bryant <shadow53@shadow53.com>
2020-10-28 01:14:33 -0700

    Convert to python3

--- on-add.habitrpg.01.py
+++ on-add.habitrpg.01.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import sys
 import json
@@ -9,12 +9,12 @@ from taskw import TaskWarrior
 URL = 'https://habitica.com/api/v3'
 
 if len(sys.argv) == 1:
-    print "The Habitica hook does not work with TaskWarrior API v1, please upgrade to Taskwarrior 2.4.3 or higher"
-    sys.exit(0)
+    print("The Habitica hook does not work with TaskWarrior API v1, please upgrade to Taskwarrior 2.4.3 or higher")
+    sys.exit(1)
 
 if not sys.argv[1] == "api:2":
-    print "The Habitica hook only supports TaskWarrior API v2 at this time"
-    sys.exit(0)
+    print("The Habitica hook only supports TaskWarrior API v2 at this time")
+    sys.exit(1)
 
 configarg = sys.argv[4]
 configfile = configarg[3:]
@@ -24,65 +24,68 @@ API_KEY = config['habitica']['api_key']
 API_USER = config['habitica']['api_user']
 
 headers = {
-		'Content-Type' : 'application/json',
-		'x-api-user' : API_USER,
-		'x-api-key' : API_KEY
-	}
+    'Content-Type': 'application/json',
+    'x-api-user': API_USER,
+    'x-api-key': API_KEY
+}
+
 
 def main():
-	jsonTask = json.loads(sys.stdin.readline())
+    jsonTask = json.loads(sys.stdin.readline())
 
-	id = pushTask(jsonTask)
-	if not id == "":
-		jsonTask["id_habitica"] = id
-		print "Task added on Habitica"
-	else:
-		print "Task was not added on Habitica"
+    id = pushTask(jsonTask)
+    if not id == "":
+        jsonTask["id_habitica"] = id
+        print("Task added on Habitica")
+    else:
+        print("Task was not added on Habitica")
 
-	print(json.dumps(jsonTask))
+    print(json.dumps(jsonTask))
+
 
 def pushTask( jsonTask ):
-	values = {
-		"type" : "todo",
-		"text" : jsonTask["description"],
-		"notes" : "Created from Taskwarrior"
-		}
+    values = {
+        "type" : "todo",
+        "text" : jsonTask["description"],
+        "notes" : "Created from Taskwarrior"
+    }
 
-	priorityMap = {
-		"trivial": .1,
-		"easy": 1,
-		"medium": 1.5,
-		"hard": 2
-	}
+    priorityMap = {
+        "trivial": .1,
+        "easy": 1,
+        "medium": 1.5,
+        "hard": 2
+    }
 
-	if 'due' in jsonTask:
-		values["date"] = jsonTask["due"]
+    if 'due' in jsonTask:
+        values["date"] = jsonTask["due"]
 
-	if 'difficulty' in jsonTask and jsonTask["difficulty"] in priorityMap:
-		values["difficulty"] = priorityMap[jsonTask["difficulty"]]
+    if 'difficulty' in jsonTask and jsonTask["difficulty"] in priorityMap:
+        values["difficulty"] = priorityMap[jsonTask["difficulty"]]
 
-	try:
-		req = requests.post(URL + '/tasks/user', data=json.dumps(values), headers=headers, timeout=10)
-		jsonHabiticaTask = json.loads(req.text)
-		value = '';
-	except requests.ConnectTimeout:
-		print "Timeout while communicating with Habitica server!"
-	except requests.ConnectionError:
-		print "Connection error while communicating with Habitica server!"
+    try:
+        req = requests.post(URL + '/tasks/user', data=json.dumps(values), headers=headers, timeout=10)
+        jsonHabiticaTask = json.loads(req.text)
+        value = '';
+    except requests.ConnectTimeout:
+        print("Timeout while communicating with Habitica server!")
+    except requests.ConnectionError:
+        print("Connection error while communicating with Habitica server!")
 
-	try:
-		vError = jsonHabiticaTask["err"]
-		print "Error while pushing task to Habitica : " + vError
-	except:
-		try:
-			value = jsonHabiticaTask["data"]["id"]
-		except:
-			value = ""
+    try:
+        vError = jsonHabiticaTask["err"]
+        print("Error while pushing task to Habitica : %s" % vError)
+    except:
+        try:
+            value = jsonHabiticaTask["data"]["id"]
+        except:
+            value = ""
 
-	return value
+    return value
+
 
 try:
-	main()
+    main()
 except:
-	print "Error! Unable to add task to Habitica"
+    print("Error! Unable to add task to Habitica")
 sys.exit(0)
